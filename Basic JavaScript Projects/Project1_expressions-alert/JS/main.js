1730cc7cc679a7096eb6240bceef97d83cd46d6c
KaityBadge <kaitybadge@yahoo.com>
2024-12-03 08:57:51 -0800

    First assignment

---
+++ Basic JavaScript Projects/Project1_expressions-alert/JS/main.js
@@ -0,0 +1,27 @@
+window.alert("Hello, World");   //This creates a pop up window that will display the words Hello World  
+
+document.write("Hello, World!! ");  //Prints text on the document
+
+var Sent1 = "This is the beginning of the string, "; //Creating a variable with a string value
+var Sent2 = "and this is the end of the string. "; //Creating another variable with a string value
+document.write(Sent1 + Sent2); //Concatenated the two variables, so the strings will make one full sentence
+
+//This portion involves concatenating strings
+document.write("Do what you want with your life, " 
+    +"because it\'s your life, "    //The plus sign is an operator that let's me add strings together
+    + "nobody lives it like you do. ");  //The strings will be combind one after another, and look like one sentence.
+
+var C = "After this comment" + ", is this comment.";  //Here I am creating a variable with a value of two strings
+document.write(C);  //Telling the computer to print the value of variable C, which will pring both strings in order
+
+
+//Here I have assigned multiple variables
+var Family = "The Badgleys", Dad = "Kaity", Mom = "Grace", Dog = "Benny";
+document.write(Dad); //Telling the computer to find a specific variable and print its value
+//This should find the variable called Dad and pring its value of Kaity 
+
+
+//This is a statement that contains an expression
+document.write(3+3); //three plus three is the expression
+// The statement is document.write() because it performs a task of writing on the document
+// The expression (3+3) will result in a value which means the computer will print the value of 6
