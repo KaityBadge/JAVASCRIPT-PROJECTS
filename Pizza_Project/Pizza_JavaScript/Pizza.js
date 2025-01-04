a8b06c18fb051289258f8cabbe8ba0dc85669230
KaityBadge <kaitybadge@yahoo.com>
2025-01-03 21:05:27 -0800

    added notes to pizza.js

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,16 +1,22 @@
 function getReceipt() { 
     //This initializes our string so it can get passed
-    //from function to function, growling line by line into a full receipt
+    //from function to function, growing line by line into a full receipt
     var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
+    //sets index to 0, 0 is less than the length of the array,
+    //i++ adds one to the index to continue through the for loop
     for (var i = 0; i < sizeArray.length; i++) {
+        //if a box is checked off for one of the pizza sizes (sizeArray)
         if (sizeArray[i].checked) {
+            //then set the variable "selectedSize" to the value of the checked box "sizeArray[i]"
             var selectedSize = sizeArray[i].value;
+            //text1 will now say "<h3>You Ordered: </h3>" + whatever size was selected 
             text1 = text1+selectedSize+"<br>";
         }
     }
+    //displays the total price for each pizza size
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
@@ -23,8 +29,11 @@ function getReceipt() {
         sizeTotal = 16;
     }
     runningTotal = sizeTotal;
+    //displays size + price in console
     console.log(selectedSize + " = $" + sizeTotal + ".00");
+    //displays "<h3>You Ordered:</h3>" and the selected pizza size
     console.log("size text1: " + text1);
+    //Display total price for pizza size
     console.log("subtotal: $" + runningTotal + ".00");
     //these variables will get passed on to each function
     getTopping(runningTotal, text1);
@@ -36,17 +45,23 @@ function getTopping(runningTotal,text1) {
     var toppingArray = document.getElementsByClassName("toppings");
     for (var j = 0; j < toppingArray.length; j++) {
         if (toppingArray[j].checked) {
+            //.push adds new items to the end of an array
+            //here .push adds toppings and updates value as toppings are selected
             selectedTopping.push(toppingArray[j].value);
+            //displays the number of selected toppings in console
             console.log("selected topping item: (" + toppingArray[j].value + ")");
             text1 = text1 + toppingArray[j].value + "<br>";
         }
     }
     var toppingCount = selectedTopping.length;
     if (toppingCount > 1) {
+        //if number or toppings is more than one, subtract 1 (one free topping)
         toppingTotal = (toppingCount - 1);
     } else {
+        //otherwise if there is only one topping, make total 0 (one free topping)
         toppingTotal = 0;
     }
+    //runningTotal is not the prize of pizza size plus price of each topping
     runningTotal = (runningTotal + toppingTotal);
     console.log("total selected topping items: " + toppingCount);
     console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
